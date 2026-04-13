a05ed6311296f4a73d4b58826183c84179157572
Moonwoven <lavya2713@gmail.com>
2026-04-13 14:21:41 +0530

    UC1 implemented

---
+++ src/main/java/uc1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class uc1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
